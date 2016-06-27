c856d2364010dd6cefb2d4a6c2378b9bbb5262e3
Rémy-Christophe Schermesser <git@schermesser.com>
2016-06-27 13:54:30 +0200

    Basic CRUD

--- back/README.md
+++ back/README.md
@@ -10,3 +10,11 @@
 ## Run (have port 8080 available)
     
     sh launch.sh
+
+## Endpoints
+
+* GET http://localhost:8080/1/events
+* GET http://localhost:8080/1/events/:name
+* POST http://localhost:8080/1/events
+* PUT http://localhost:8080/1/events/:name
+* DELETE http://localhost:8080/1/events/:name
--- back/src/main/scala/polyconf16/api/EventsController.scala
+++ back/src/main/scala/polyconf16/api/EventsController.scala
@@ -2,28 +2,45 @@ package polyconf16.api
 
 import polyconf16.models.Event
 
+import scala.collection.mutable
+
 class EventsController extends BaseController {
 
-  get("/") {
-    Event.default
+  val events = mutable.ArrayBuffer.empty ++ Event.default
+
+  def getEvent(name: String): Option[Event] = {
+    events.find(_.name == name)
   }
 
-  get("/:id") {
+  def findIndex(name: String): Option[Int] = {
+    val index = events.indexWhere(_.name == name)
+    if (index == -1) {
+      None
+    } else {
+      Some(index)
+    }
+  }
 
+  get("/") {
+    events
+  }
+
+  get("/:name") {
+    getEvent(params("name")).fold(halt(404))(e => e)
   }
 
   post("/") {
-
+    events += parsedBody.extract[Event]
   }
 
-  put("/:id") {
-
+  put("/:name") {
+    val index = findIndex(params("name")).getOrElse(halt(404))
+    events.update(index, parsedBody.extract[Event])
   }
 
-  //  name: string,
-  //  participants: [string],
-  //  start: time,
-  //  end: time,
-  //  type: [emoji]
+  delete("/:name") {
+    val index = findIndex(params("name")).getOrElse(halt(404))
+    events.remove(index)
+  }
 
 }
